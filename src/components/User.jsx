26a9a0f0afb80fb5a9daa492f57b17aa79c24b72
gureenkov56 <gureenkov56@gmail.com>
2023-02-17 10:53:52 +0400

    Get users by api

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import '../styles/user.scss'
 import UIButton from './UI/UIButton'
 
-function User() {
+function User({info}) {
 
     return (
         <>
             <section className='User'>
                 <div className="avatar">
-                    <img src="https://robohash.org/hicveldicta.png" alt="avatar" />
+                    <img src={info.image} alt="avatar" />
                 </div>
                 <div className="info">
-                    <h2 className='info__fullname'>Terry Smith</h2>
-                    <div className="info__age">23 y.o</div>
-                    <div className="info__location">Washington</div>
+                    <h2 className='info__fullname'>{info.firstName} {info.lastName}</h2>
+                    <div className="info__age">{info.age} y.o</div>
+                    <div className="info__location">{info.address.city}</div>
                 </div>
                 <div className="buttons">
                     <UIButton color='primary'
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import User from './User'
 
 function UserList() {
 
+    const [UserList, setUserList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/users')
+            .then(res => res.json())
+            .then(
+                (res) => {
+                    setUserList(res.users);
+                    console.log(res);
+                },
+                (error) => {
+                    console.log('error')
+                }
+            )
+    }, [])
+
     return (
         <>
-        <User/>
-        <User/>
-        <User/>
-        <User/>
+            {UserList.map(user => 
+                <User info={user} />
+            ) }
         </>
     )
 }
